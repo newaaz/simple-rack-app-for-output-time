10220f2dc66bf9b38e7b18ac4071038a4aedef95
aaz <ilk-ui@yandex.ru>
2022-06-29 18:32:48 +0300

    re-init repo

--- app.rb
+++ app.rb
@@ -1,14 +1,1 @@
-class App  
-  def call(env)
-    request = Rack::Request.new(env)
-    line_to_time = TimeHandler.new(request.params["format"].split(','))
 
-    build_responce(line_to_time)
-  end
-
-  def build_responce(line_to_time)
-    responce = Rack::Response.new(line_to_time.format[:body])
-    responce.status = line_to_time.format[:status]
-    responce.finish
-  end
-end
--- config.ru
+++
@@ -1,9 +0,0 @@
-require_relative 'time_handler'
-require_relative 'app'
-
-use Rack::Reloader, 0
-use Rack::ContentType, 'text/html'
-
-map '/time' do
-  run App.new
-end
